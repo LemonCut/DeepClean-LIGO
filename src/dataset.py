7ebe3894db8df0c41ce0d2102e55a1c1bed0e556
h1zia <h1zia@ucsd.edu>
2025-12-11 04:38:49 -0800

    Fix preprocessing: Don't filter witness data for nonlinear couplings

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,8 @@ from torch.utils.data import Dataset
 from scipy.signal import butter, sosfilt
 
 class GWDataset(Dataset):
-    def __init__(self, strain, witnesses, sample_rate, segment_duration=8.0, overlap=7.75, band_start=55.0, band_end=65.0):
+    def __init__(self, strain, witnesses, sample_rate, segment_duration=8.0, overlap=7.75, band_start=55.0, band_end=65.0,
+                 linear=True):
         """
         strain: array of wave data
         witnesses: matrix of witness channels (N_channels, Time)
@@ -14,13 +15,18 @@ class GWDataset(Dataset):
         self.step = int((segment_duration - overlap) * sample_rate) 
         
         sos = butter(8, [band_start, band_end], btype='bandpass', fs=sample_rate, output='sos')
-        witnesses_filtered = np.zeros_like(witnesses)
-        for i in range(witnesses.shape[0]):
-            witnesses_filtered[i] = sosfilt(sos, witnesses[i])
+        strain_filtered = sosfilt(sos, strain)
+
+        if linear:
+            witnesses_filtered = np.zeros_like(witnesses)
+            for i in range(witnesses.shape[0]):
+                witnesses_filtered[i] = sosfilt(sos, witnesses[i])
+        else:
+            witnesses_filtered = witnesses
             
-        self.strain_mean = np.mean(strain)
-        self.strain_std = np.std(strain)
-        self.strain = (strain - self.strain_mean) / self.strain_std
+        self.strain_mean = np.mean(strain_filtered)
+        self.strain_std = np.std(strain_filtered)
+        self.strain = (strain_filtered - self.strain_mean) / self.strain_std
         
         self.wit_mean = np.mean(witnesses_filtered, axis=1, keepdims=True)
         self.wit_std = np.std(witnesses_filtered, axis=1, keepdims=True)
